99aab5ae1bf7db23a30b201798ccc8b101a5eb6e
harini-y-ops <harini.n20061@gmail.com>
2026-03-02 19:41:14 +0530

    Create create node manually.py

---
+++ linked-list/create node manually.py
@@ -0,0 +1,19 @@
+#Create a node manually in a linked list 
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+node1 = Node(10)
+node2 = Node(20)
+node3 = Node(30)
+
+node1.next = node2
+node2.next = node3
+
+temp = node1
+while temp:
+    print(temp.data, end=" → ")
+    temp = temp.____
+
+print("None")
